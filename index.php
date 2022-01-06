d035ae7ff9136d0802f8fdad166477ed73722b14
Shano1993 <stefan.hanotiau@gmail.com>
2022-01-06 15:35:39 +0100

    php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Exo 132</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<?php
+    $text = "Du texte est stocké";
+    echo $text . "<br>";
+    $y = 30;
+    echo "Y vaux $y" . "<br>";
+    $m = 5;
+    $n = 7;
+    echo $m + $n;
+?>
+
+</body>
+</html>
